aa0c0ade7b0ea4ef013be9a19e8648331f33ebe0
Marek Dano <mk.dano@gmail.com>
2018-09-09 23:25:03 +0100

    Add a navbar with a drawer to the main page.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,22 +1,93 @@
 import * as React from 'react';
-import './App.css';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+import { 
+  AppBar,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar, 
+  Typography
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
 
-import logo from '../logo.svg';
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  list: {
+    width: 250,
+  },
+};
 
-class App extends React.Component {
-  public render() {
+interface IState {
+  openDrawer: boolean;
+};
+
+class App extends React.Component<WithStyles<typeof styles>, IState> {
+  state = {
+    openDrawer: false,
+  };
+
+  toggleDrawer = (open: boolean) => () => {
+    this.setState({
+      openDrawer: open,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu" onClick={this.toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer open={this.state.openDrawer} onClose={this.toggleDrawer(false)}>
+              <div
+                tabIndex={0}
+                role="button"
+                className={classes.list}
+                onClick={this.toggleDrawer(false)}
+                onKeyDown={this.toggleDrawer(false)}
+              >
+                <List>
+                  <ListItem button={true}>
+                    <ListItemIcon>
+                      <InboxIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Inbox" />
+                  </ListItem>
+                  <ListItem button={true}>
+                    <ListItemIcon>
+                      <DraftsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Drafts" />
+                  </ListItem>
+                </List>
+              </div>
+            </Drawer>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              RecordHazard
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
