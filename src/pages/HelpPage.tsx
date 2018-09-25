3b86c01efe7502c3f265bac0b9bea88b59cf2906
Marek Dano <mk.dano@gmail.com>
2018-09-25 23:35:14 +0100

    Add HelpPage component with layout.

---
+++ src/pages/HelpPage.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { Typography, Theme, withStyles, WithStyles, Button } from '@material-ui/core';
+import PhoneIcon from '@material-ui/icons/Phone';
+import MailIcon from '@material-ui/icons/Mail';
+
+const styles = (theme: Theme) => ({
+	mainContent: {
+  	maxWidth: 600,
+    margin: '0 auto',
+    padding: '1.8em 0 0',
+	},
+	contact: {
+		marginTop: theme.spacing.unit * 5
+	},
+	icon: {
+		margin: theme.spacing.unit
+	}
+});
+
+const HelpPage = ({classes}: WithStyles<typeof styles>) => {
+	return (
+		<div className={classes.mainContent}>
+			<Typography variant="title" color="textSecondary">
+				Need help?
+			</Typography>
+			<Typography variant="body2">
+				Prevent your workspace to be knocked down and report each hazard!
+      </Typography>
+			<div className={classes.contact}>
+				<Typography variant="headline">
+					Contact us!
+				</Typography>
+				<Button variant="fab" color="primary" className={classes.icon}>
+					<PhoneIcon />
+				</Button>
+				<Button variant="fab" color="primary" className={classes.icon} >
+					<MailIcon />
+				</Button>
+			</div>
+			
+		</div>
+	
+	);
+};
+
+export default withStyles(styles, { withTheme: true })(HelpPage);
