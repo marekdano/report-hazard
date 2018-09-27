d182eaf98d3742884efd0f1dc0db760eb69b32b1
Marek Dano <mk.dano@gmail.com>
2018-09-27 07:54:48 +0100

    Wrap main component with router. Move navigation code from App component.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,48 +1,19 @@
 import * as React from 'react';
 import { withStyles, WithStyles, Theme } from '@material-ui/core/styles';
-import { 
-  AppBar,
-  Button,
-  Drawer,
-  IconButton,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Toolbar, 
-  Typography
-} from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import RssFeedIcon from '@material-ui/icons/RssFeed';
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import { Button, Typography } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
 import ReportForm from './ReportForm';
+import Navigation from './components/Navigation';
 
 const styles = (theme: Theme) => ({
   root: {
-    flexGrow: 1,
-  },
-  flex: {
     flexGrow: 1,
   },
   mainContent: {
     maxWidth: 600,
     margin: '0 auto',
     padding: '1.8em 0 0',
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    padding: '20px 0 5px 24px',
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  list: {
-    width: 250,
   },
   fab: {
     position: 'absolute' as 'absolute',
@@ -79,56 +50,7 @@ class App extends React.Component<WithStyles<typeof styles>, IState> {
 
     return (
       <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton 
-              className={classes.menuButton} 
-              color="inherit" aria-label="Menu" 
-              onClick={this.toggleDrawer(true)}>
-              <MenuIcon />
-            </IconButton>
-            <Drawer open={this.state.openDrawer} onClose={this.toggleDrawer(false)}>
-              <div
-                tabIndex={0}
-                role="button"
-                className={classes.list}
-                onClick={this.toggleDrawer(false)}
-                onKeyDown={this.toggleDrawer(false)}
-              >
-                <div className={classes.drawerHeader}>
-                  <Typography variant="title">
-                    RecordHazard
-                  </Typography>
-                </div>
-                <List>
-                  <ListItem button={true}>
-                    <ListItemIcon>
-                      <RssFeedIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Feed" />
-                  </ListItem>
-                  <ListItem button={true}>
-                    <ListItemIcon>
-                      <HelpOutlineIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Help" />
-                  </ListItem>
-                  <ListItem>
-                    <Button variant="contained" color="secondary">
-                      Enable Notifications
-                    </Button>
-                  </ListItem>
-                </List>
-              </div>
-            </Drawer>
-            <Typography variant="title" color="inherit" className={classes.flex}>
-              RecordHazard
-            </Typography>
-            <Button variant="contained" color="secondary">
-              Enable Notifications
-            </Button> 
-          </Toolbar>
-        </AppBar>
+        <Navigation openDrawer={this.state.openDrawer} toggleDrawer={this.toggleDrawer} />
         <main>
           <div className={classes.mainContent}>
             {this.state.openReportForm ?
@@ -139,10 +61,9 @@ class App extends React.Component<WithStyles<typeof styles>, IState> {
           </div>
           
           {!this.state.openReportForm && 
-          <Button variant="fab" color="secondary" className={classes.fab} aria-label="Add" onClick={this.toggleReportForm(true)} >
-            <AddIcon />
-          </Button>}
-        
+            <Button variant="fab" color="secondary" className={classes.fab} aria-label="Add" onClick={this.toggleReportForm(true)} >
+              <AddIcon />
+            </Button>}
         </main>
       </div>
     );
